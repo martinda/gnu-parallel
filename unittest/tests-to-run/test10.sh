f38da581b79be366abc8df7bfd4722c1c4a541d5
Ole Tange <ole@tange.dk>
2009-03-12 03:07:08 +0100

    Better unittest for -x

--- unittest/tests-to-run/test10.sh
+++ unittest/tests-to-run/test10.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # This causes problems if we kill child processes
-
 seq 1 40 | parallel -j 0 seq 1 10  | sort |md5sum
 seq 1 40 | parallel -j 0 seq 1 10 '| parallel -j 3 echo' | sort |md5sum
 
 # Test of xargs
-seq 1 30000 | parallel echo  | sort |md5sum
+seq 1 80000 | parallel -x echo  | mop -d 4 "|sort |md5sum" "| wc" 
