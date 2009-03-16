d78f8539f9a8be447315f6e399a69b0718acfe24
Ole Tange <ole@tange.dk>
2009-03-16 08:58:39 +0100

    Implemented -X. Fixed bug with -x:   If the command line had more than one {} it could result in a too   long line

--- unittest/tests-to-run/test10.sh
+++ unittest/tests-to-run/test10.sh
@@ -5,4 +5,13 @@ seq 1 40 | parallel -j 0 seq 1 10  | sort |md5sum
 seq 1 40 | parallel -j 0 seq 1 10 '| parallel -j 3 echo' | sort |md5sum
 
 # Test of xargs
-seq 1 80000 | parallel -x echo  | mop -d 4 "|sort |md5sum" "| wc" 
+seq 1 60000 | parallel -x echo  | mop -d 4 "|sort |md5sum" "| wc" 
+(echo foo;echo bar) | parallel -x echo 1{}2{}3 A{}B{}C
+(echo foo;echo bar) | parallel -X echo 1{}2{}3 A{}B{}C
+seq 1 60000 | parallel -x echo 1{}2{}3 | mop -d 4 "|sort |md5sum" "| wc" 
+seq 1 60000 | parallel -x echo 1{}2{}3 | \
+  mop -q "|sort |md5sum" :parallel
+echo -n "Chars per line: "
+CHAR=$(cat ~/.mop/:parallel | wc -c)
+LINES=$(cat ~/.mop/:parallel | wc -l)
+echo "$CHAR/$LINES" | bc
