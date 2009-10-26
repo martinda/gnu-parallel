5059b5453a003c8c99330fff161b9c8d638fc7b5
Ole Tange <ole@tange.dk>
2009-10-26 12:22:21 +0100

    SIGUSR1 tells parallel to wait for running jobs to finish, but not start new jobs. Lazy evaluation of max_length_of_command_line and no_of_cpus

--- unittest/tests-to-run/test13.sh
+++ unittest/tests-to-run/test13.sh
@@ -4,3 +4,6 @@
 ulimit -n 50
 (echo "sleep 3; echo begin"; seq 1 30 | parallel -kq echo "sleep 1; echo {}"; echo "echo end") \
 | parallel -k -j0
+
+# Test SIGUSR1
+(sleep 5; killall parallel -USR1) & seq 1 100 | parallel -k sleep 3';' echo
