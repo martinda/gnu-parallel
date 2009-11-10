fcfc9f6d7025ee5f7fe0930ece24deecf2ef4434
Ole Tange <ole@tange.dk>
2009-11-10 15:14:15 +0100

    parallel: Implemented replacement string -I similar to xargs -I

--- unittest/tests-to-run/test10.sh
+++ unittest/tests-to-run/test10.sh
@@ -8,8 +8,8 @@ seq 1 40 | parallel -j 0 seq 1 10 '| parallel -j 3 echo' | sort |md5sum
 seq 1 60000 | parallel -x echo  | mop -d 4 "|sort |md5sum" "| wc" 
 (echo foo;echo bar) | parallel -x echo 1{}2{}3 A{}B{}C
 (echo foo;echo bar) | parallel -X echo 1{}2{}3 A{}B{}C
-seq 1 60000 | parallel -x echo 1{}2{}3 | mop -d 4 "|sort |md5sum" "| wc" 
-seq 1 60000 | parallel -x echo 1{}2{}3 | \
+seq 1 60000 | parallel -x echo a{}b{}c | mop -d 4 "|sort |md5sum" "| wc" 
+seq 1 60000 | parallel -x echo a{}b{}c | \
   mop -q "|sort |md5sum" :parallel
 echo -n "Chars per line: "
 CHAR=$(cat ~/.mop/:parallel | wc -c)
---
+++ unittest/tests-to-run/test14.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Test -I
+seq 1 10 | parallel -k 'seq 1 {} | parallel -k -I :: echo {} ::'
+
+seq 1 10 | parallel -k 'seq 1 {} | parallel -X -k -I :: echo a{} b::'
+
+seq 1 10 | parallel -k 'seq 1 {} | parallel -x -k -I :: echo a{} b::'
+
+seq 1 60000 | parallel -I :: -x echo a::b::c | \
+  mop -q "|sort |md5sum" :parallel
+echo -n "Chars per line: "
+CHAR=$(cat ~/.mop/:parallel | wc -c)
+LINES=$(cat ~/.mop/:parallel | wc -l)
+echo "$CHAR/$LINES" | bc
+
+seq 1 60000 | parallel -I :: -X echo a::b::c | \
+  mop -q "|sort |md5sum" :parallel
+echo -n "Chars per line: "
+CHAR=$(cat ~/.mop/:parallel | wc -c)
+LINES=$(cat ~/.mop/:parallel | wc -l)
+echo "$CHAR/$LINES" | bc
