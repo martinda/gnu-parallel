aaa69ffa1b8868f9aa0940861a6932491bb863a5
Ole Tange <ole@tange.dk>
2010-02-03 15:23:20 +0100

    parallel: Converted to Getopt::Long. Implemented -TERM for shutdown.

--- unittest/tests-to-run/test13.sh
+++ unittest/tests-to-run/test13.sh
@@ -5,5 +5,5 @@ ulimit -n 50
 (echo "sleep 3; echo begin"; seq 1 30 | parallel -kq echo "sleep 1; echo {}"; echo "echo end") \
 | parallel -k -j0
 
-# Test SIGUSR1
-(sleep 5; killall parallel -USR1) & seq 1 100 | parallel -k sleep 3';' echo
+# Test SIGTERM
+(sleep 5; killall parallel -TERM) & seq 1 100 | parallel -k sleep 3';' echo
