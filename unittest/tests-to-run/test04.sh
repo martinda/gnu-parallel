555436f7461624a0f61a05330e84ee8e117b05b9
Ole Tange <ole@tange.dk>
2010-07-09 15:24:24 +0200

    Unittest: -0 on filenames ending in \n

--- unittest/tests-to-run/test04.sh
+++ unittest/tests-to-run/test04.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PAR=parallel
-
 rm -rf tmp 2>/dev/null
 cd input-files
 tar xjf random_dirs_no_newline.tar.bz2 
@@ -9,27 +7,34 @@ cd ..
 cp -a input-files/random_dirs_no_newline tmp
 cd tmp
 
-# tests if special dir names causes problems
-ls | $PAR -v touch -- {}/abc-{}-{} 2>&1 | perl -e 'print sort (<>)' | md5sum
+echo '### Tests if special dir names causes problems'
+ls | parallel -v touch -- {}/abc-{}-{} 2>&1 | perl -e 'print sort (<>)' | md5sum
 echo -n 'There are ' 
 find . -type d -print0 | perl -0 -ne '$a++;END{print $a}'
 echo -n ' dirs with '
 find . -type f -print0 | perl -0 -ne '$a++;END{print $a}'
 echo ' files'
 echo 'Removing files'
-ls | $PAR -v rm -- {}/abc-{}-{} 2>&1 | perl -e 'print sort (<>)' | md5sum
+ls | parallel -v rm -- {}/abc-{}-{} 2>&1 | perl -e 'print sort (<>)' | md5sum
 echo -n 'There are ' 
 find . -type d -print0 | perl -0 -ne '$a++;END{print $a}'
 echo -n ' dirs with '
 find . -type f -print0 | perl -0 -ne '$a++;END{print $a}'
 echo ' files'
 echo 'Removing dirs'
-ls | $PAR -v rmdir -- {} 2>&1 | perl -e 'print sort (<>)' | md5sum
+ls | parallel -v rmdir -- {} 2>&1 | perl -e 'print sort (<>)' | md5sum
 echo -n 'There are ' 
 find . -type d -print0 | perl -0 -ne '$a++;END{print $a}'
 echo -n ' dirs with '
 find . -type f -print0 | perl -0 -ne '$a++;END{print $a}'
 echo ' files'
+echo '### Tests name ending in \n causes problems'
+echo newline1 >'newline1
+'
+echo newline2 >'newline2
+'
+find newli* -print0 | parallel -0 cat {} ">" {}.bak
+find newli* -print0 | sort -z
 
 cd ..
 rm -rf tmp
