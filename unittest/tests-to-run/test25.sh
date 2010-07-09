ff229d0bd434ce87034f1929354082f862cb03f2
Ole Tange <ole@tange.dk>
2010-07-09 22:37:45 +0200

    Easier calc of max_command_length. Convert Global:: to Private::. Bugfix: ETA for args given on command line.

--- unittest/tests-to-run/test25.sh
+++ unittest/tests-to-run/test25.sh
@@ -12,3 +12,8 @@ echo via first cat |parallel -kv cat ::: - -
 echo via cat |parallel --arg-sep .--- -kv .--- 'cat' 'echo b'
 echo via cat |parallel -kv ::: 'cat' 'echo b'
 echo no output |parallel -kv ::: 'echo a' 'cat'
+
+echo '### Bug made 4 5 go before 1 2 3'
+parallel -k ::: "sleep 1; echo 1" "echo 2" "echo 3" "echo 4" "echo 5"
+echo '### Bug made 3 go before 1 2'
+parallel -kj 1 ::: "sleep 1; echo 1" "echo 2" "echo 3"
