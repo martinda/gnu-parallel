fcbfd192201fd9fa7dd484df9d2183fe6bd23f6c
Ole Tange <ole@tange.dk>
2010-08-01 20:09:31 +0200

    Example for manual. Bug fix. More unittests.

--- unittest/tests-to-run/test29.sh
+++ unittest/tests-to-run/test29.sh
@@ -9,6 +9,20 @@ echo '### Test of quoting of > bug if line continuation'
 echo '### Test of --trim illegal'
 stdout parallel --trim fj ::: echo
 
+echo '### Test of eof string on :::'
+parallel -E ole echo ::: foo ole bar
+
+echo '### Test of ignore-empty string on :::'
+parallel -r echo ::: foo '' ole bar
+
+echo '### Test of trailing space continuation'
+(echo foo; echo '';echo 'ole ';echo bar;echo quux) | xargs -r -L2 echo
+(echo foo; echo '';echo 'ole ';echo bar;echo quux) | parallel -kr -L2 echo
+parallel -kr -L2 echo ::: foo '' 'ole ' bar quux
+(echo foo; echo '';echo 'ole ';echo bar;echo quux) | xargs -r -L2 -E bar echo
+(echo foo; echo '';echo 'ole ';echo bar;echo quux) | parallel -kr -L2 -E bar echo
+parallel -kr -L2 -E bar echo ::: foo '' 'ole ' bar quux
+
 echo '### Test of --colsep'
 echo 'a%c%b' | parallel --colsep % echo {1} {3} {2}
 (echo 'a%c%b'; echo a%c%b%d) | parallel -k --colsep % echo {1} {3} {2} {4}
@@ -18,6 +32,13 @@ parallel -k --colsep % echo {1} {3} {2} ::: a%c%b d%f%e
 parallel -k --colsep % echo {1} {3} {2} ::: a%c%b
 
 parallel -k --colsep % echo {1} {3} {2} {4} ::: a%c%b a%c%b%d
+
+echo '### Test of tab as colsep'
+printf 'def\tabc\njkl\tghi' | parallel -k --colsep '\t' echo {2} {1}
+parallel -k -a <(printf 'def\tabc\njkl\tghi') --colsep '\t' echo {2} {1}
+
+echo '### Test of quoting after colsplit'
+parallel --colsep % echo {2} {1} ::: '>/dev/null%>/tmp/null'
 
 echo '### Test of --colsep as regexp'
 (echo 'a%c%%b'; echo a%c%b%d) | parallel -k --colsep %+ echo {1} {3} {2} {4}
