b26dbadcc053eb8b535510468b372da2aa5ee4ae
Ole Tange <ole@tange.dk>
2010-08-26 20:03:59 +0200

    Bugfix: --eta/--progress with 0 jobs gave division by zero. Bugfix in Makefile by Piotr Jaroszyński <p dot jaroszynski at gmail dot com>

--- unittest/tests-to-run/test30.sh
+++ unittest/tests-to-run/test30.sh
@@ -3,5 +3,11 @@
 echo '### Test of --eta'
 seq 1 10 | stdout parallel --eta "sleep 1; echo {}" | wc -l
 
+echo '### Test of --eta with no jobs'
+stdout parallel --eta "sleep 1; echo {}" < /dev/null
+
 echo '### Test of --progress'
 seq 1 10 | stdout parallel --progress "sleep 1; echo {}" | wc -l
+
+echo '### Test of --progress with no jobs'
+stdout parallel --progress "sleep 1; echo {}" < /dev/null
