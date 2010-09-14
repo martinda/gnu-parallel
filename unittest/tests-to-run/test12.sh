ceca2fea7546419d31177b4220d80e52cbf505fb
Ole Tange <ole@tange.dk>
2010-09-14 21:00:20 +0200

    unittest: using niceload for test12 my machine is not completely unusable during unittests

--- unittest/tests-to-run/test12.sh
+++ unittest/tests-to-run/test12.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PAR=parallel
-
 # Test if we can deal with output > 4 GB
-echo | $PAR 'dd if=/dev/zero count=43 bs=100000k; echo 1; echo 2' | md5sum
+echo | niceload -l=1.5 parallel -q perl -e '$a="x"x1000000;for(0..4300){print $a}' | md5sum
+# dd does not work with niceload (no idea why)
+#echo | parallel 'dd if=/dev/zero count=43 bs=100000k; echo 1; echo 2' | md5sum
