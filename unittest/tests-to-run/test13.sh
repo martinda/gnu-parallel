59cb631264cfcc203603ffec1e354a14faca7bc5
Ole Tange <ole@tange.dk>
2010-09-21 22:00:30 +0200

    Bugfix: --keeporder with -j100% mixed up order of first job parallel: Unittest for --retries. Passes unittest.

--- unittest/tests-to-run/test13.sh
+++ unittest/tests-to-run/test13.sh
@@ -5,5 +5,11 @@ ulimit -n 50
 (echo "sleep 3; echo begin"; seq 1 30 | parallel -kq echo "sleep 1; echo {}"; echo "echo end") \
 | parallel -k -j0
 
+echo '### Test --keep-order'
+(seq 0 2) | parallel --keep-order -j100% -S 1/:,2/parallel@server2 -q perl -e 'sleep 1;print "job{}\n";exit({})'
+
+echo '### Test --keeporder'
+(seq 0 2) | parallel --keeporder -j100% -S 1/:,2/parallel@server2 -q perl -e 'sleep 1;print "job{}\n";exit({})'
+
 echo '### Test SIGTERM'
 (sleep 5; killall parallel -TERM) & seq 1 100 | parallel -k sleep 3';' echo
--- unittest/tests-to-run/test20.sh
+++ unittest/tests-to-run/test20.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-PAR=parallel
-
 echo '### Test --number-of-cpus'
-$PAR --number-of-cpus
+parallel --number-of-cpus
 
 echo '### Test --number-of-cores'
-$PAR --number-of-cores
+parallel --number-of-cores
 
 echo '### Test --use-cpus-instead-of-cores'
-(seq 1 4 | $PAR --use-cpus-instead-of-cores -j100% sleep) && echo CPUs done &
-(seq 1 4 | $PAR -j100% sleep) && echo cores done &
+(seq 1 4 | parallel --use-cpus-instead-of-cores -j100% sleep) && echo CPUs done &
+(seq 1 4 | parallel -j100% sleep) && echo cores done &
 echo 'Cores should complete first on machines with less than 4 physical CPUs'
 wait
 
