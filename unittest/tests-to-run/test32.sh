e06f6d52ce7b08289e1e11c944abe5c9ef555805
Ole Tange <ole@tange.dk>
2010-09-22 01:17:05 +0200

    parallel: Added difference between paexec

---
+++ unittest/tests-to-run/test32.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo '### Test of --retries'
+seq 1 10 | parallel --retries 2 -v -S 4.3.2.1,: echo
+
+echo '### Test of --retries - it should run 13 jobs in total'
+seq 0 12 | parallel --progress -kj100% --retries 1 -S 12/nlv.pi.dk,1/:,parallel@server2 -vq \
+ perl -e 'sleep 1;print "job{}\n";exit({})' | tail -n1 | \
+ perl -ne '@a=(split /\//,$_); print $a[1]+$a[4]+$a[7],"\n"'
+
+echo '### Test of --retries - it should run 25 jobs in total'
+seq 0 12 | parallel --progress -kj100% --retries 2 -S 12/nlv.pi.dk,1/:,parallel@server2 -vq \
+ perl -e 'sleep 1;print "job{}\n";exit({})' | tail -n1 | \
+ perl -ne '@a=(split /\//,$_); print $a[1]+$a[4]+$a[7],"\n"'
+
+echo '### Test of --retries - it should run 49 jobs in total'
+seq 0 12 | parallel --progress -kj100% --retries 4 -S 12/nlv.pi.dk,1/:,parallel@server2 -vq \
+ perl -e 'sleep 1;print "job{}\n";exit({})' | tail -n1 | \
+ perl -ne '@a=(split /\//,$_); print $a[1]+$a[4]+$a[7],"\n"'
+
