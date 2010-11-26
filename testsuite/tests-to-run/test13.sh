ae08cc3b1c54baf96e3a2dfad6beb1363bffa522
Ole Tange <ole@tange.dk>
2010-11-26 01:45:04 +0100

    parallel: SSHLogin rewritten to OO. Minor failures in test32 and test34.

--- testsuite/tests-to-run/test13.sh
+++ testsuite/tests-to-run/test13.sh
@@ -6,10 +6,10 @@ ulimit -n 50
 | stdout parallel -k -j0
 
 echo '### Test --keep-order'
-(seq 0 2) | parallel --keep-order -j100% -S 1/:,2/parallel@server2 -q perl -e 'sleep 1;print "job{}\n";exit({})'
+(seq 0 2) | parallel --keep-order -j100% -S 1/:,2/parallel@parallel-server2 -q perl -e 'sleep 1;print "job{}\n";exit({})'
 
 echo '### Test --keeporder'
-(seq 0 2) | parallel --keeporder -j100% -S 1/:,2/parallel@server2 -q perl -e 'sleep 1;print "job{}\n";exit({})'
+(seq 0 2) | parallel --keeporder -j100% -S 1/:,2/parallel@parallel-server2 -q perl -e 'sleep 1;print "job{}\n";exit({})'
 
 echo '### Test SIGTERM'
 (sleep 5; killall parallel -TERM) & seq 1 100 | stdout parallel -k sleep 3';' echo | sort
