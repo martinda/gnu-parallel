1cc9c990a43181a0a9eb6a7ff1e99991a968a4c2
Ole Tange <ole@tange.dk>
2011-01-05 18:10:58 +0100

    parallel: fixed: seq 9 | parallel -N 10 echo

--- testsuite/tests-to-run/test48.sh
+++ testsuite/tests-to-run/test48.sh
@@ -9,5 +9,12 @@ seq 1 92 | parallel -j+3 -kX -s 100 echo
 echo '### Test distribute arguments at EOF to infinity jobslots'
 seq 1 92 | parallel -j0 -kX -s 100 echo
 
+echo '### Test -N is not broken by distribution - single line'
+seq 9 | parallel  -N 10  echo
+
+echo '### Test -N is not broken by distribution - two lines'
+seq 19 | parallel  -N 10  echo
+
+
 
 
