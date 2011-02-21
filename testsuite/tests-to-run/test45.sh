14460475793627a33924968fb3e57065f1cf50b3
Ole Tange <ole@tange.dk>
2011-02-21 22:57:58 +0100

    faster forcing the load > 10 to test of --load

--- testsuite/tests-to-run/test45.sh
+++ testsuite/tests-to-run/test45.sh
@@ -7,9 +7,20 @@ echo '### Bug in --load'
 parallel -k --load 30 sleep 0.1\;echo ::: 1 2 3
 
 echo '### Test --load locally'
-seq 1 300 | nice timeout -k 1 14 parallel -j0 burnP6
+# This will force the loadavg > 10
+while cat /proc/loadavg | egrep -q '^.\.' ; do
+  (nice -n 19 burnP6 &) 2>/dev/null
+  sleep .05;
+done
+killall -9 burnP6 2>/dev/null ;sleep 1;killall -9 burnP6 2>/dev/null
 stdout /usr/bin/time -f %e parallel --load 10 sleep ::: 1 | perl -ne '$_ > 10 and print "OK\n"'
 
 echo '### Test --load remote'
-seq 1 300 | ssh parallel@$SERVER2 nice timeout -k 1 14 parallel -j0 burnP6
+# This will force the loadavg > 10
+ssh parallel@$SERVER2 "while cat /proc/loadavg | egrep -q '^.\.' ; do
+  (nice -n 19 burnP6 &) 2>/dev/null
+  sleep .05;
+done
+killall -9 burnP6 2>/dev/null ;sleep 1;killall -9 burnP6 2>/dev/null
+"
 stdout /usr/bin/time -f %e parallel -S parallel@$SERVER2 --load 10 sleep ::: 1 | perl -ne '$_ > 10 and print "OK\n"'
