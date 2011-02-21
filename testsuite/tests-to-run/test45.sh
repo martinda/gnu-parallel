bad6dd47875b0d31ed4c4cd2e69a32c878c9b669
Ole Tange <ole@tange.dk>
2011-02-21 22:29:55 +0100

    Fixes bug #32563: includes testsuite.

--- testsuite/tests-to-run/test45.sh
+++ testsuite/tests-to-run/test45.sh
@@ -3,6 +3,9 @@
 SERVER1=parallel-server3
 SERVER2=parallel-server2
 
+echo '### Bug in --load'
+parallel -k --load 30 sleep 0.1\;echo ::: 1 2 3
+
 echo '### Test --load locally'
 seq 1 300 | nice timeout -k 1 14 parallel -j0 burnP6
 stdout /usr/bin/time -f %e parallel --load 10 sleep ::: 1 | perl -ne '$_ > 10 and print "OK\n"'
