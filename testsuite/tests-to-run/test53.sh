69201240dc57c34a1fab6360c35ca1f5202ac7f0
Ole Tange <ole@tange.dk>
2011-02-23 16:22:08 +0100

    parallel: manpage update for -l 0 -L 0 -n 0, and -N 0.

--- testsuite/tests-to-run/test53.sh
+++ testsuite/tests-to-run/test53.sh
@@ -5,3 +5,9 @@ seq 1 2 | parallel -n0 echo n0
 seq 1 2 | parallel -L0 echo L0
 seq 1 2 | parallel -l0 echo l0
 seq 1 2 | parallel -N0 echo N0
+
+echo '### Test replace {}'
+seq 1 2 | parallel -N0 echo replace {} curlies
+
+echo '### Test arguments on commandline'
+parallel -N0 echo args on cmdline ::: 1 2
