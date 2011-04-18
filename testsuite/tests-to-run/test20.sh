3ab1fc535cc5c0481ed5861e24195a1070d89ba3
Ole Tange <ole@tange.dk>
2011-04-18 11:37:34 +0200

    parallel: CPU detection code for AIX thanks to Christian Netrwal.

--- testsuite/tests-to-run/test20.sh
+++ testsuite/tests-to-run/test20.sh
@@ -7,8 +7,8 @@ echo '### Test --number-of-cores'
 parallel --number-of-cores
 
 echo '### Test --use-cpus-instead-of-cores'
-(seq 1 4 | parallel --use-cpus-instead-of-cores -j100% sleep) && echo CPUs done &
-(seq 1 4 | parallel -j100% sleep) && echo cores done &
+(seq 1 4 | stdout parallel --use-cpus-instead-of-cores -j100% sleep) && echo CPUs done &
+(seq 1 4 | stdout parallel -j100% sleep) && echo cores done &
 echo 'Cores should complete first on machines with less than 4 physical CPUs'
 wait
 
