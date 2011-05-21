59ddc7b75dca9abf9d5c104d5c1a13f8f4bbb6a6
Ole Tange <ole@tange.dk>
2011-05-21 08:42:10 +0200

    parallel: Fixed https://savannah.gnu.org/bugs/index.php?33352 Passes testsuite.

---
+++ testsuite/tests-to-run/test59.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo '### Test bug https://savannah.gnu.org/bugs/index.php?33352'
+
+# produce input slowly to parallel so that it will reap a process
+# while blocking in read()
+
+MD5=md5sum
+PAR="parallel -j2 --pipe --keeporder --block 150000 --tmpdir=/dev/shm"
+perl -e '@x=1 .. 17000; for(1..100) { print "@x\n"}' | md5sum
+nice nice perl -e '@x=1 .. 17000; for(1..100) { print "@x\n"}' | pv -qL 1000000 | \
+  $PAR cat | md5sum &
+nice nice perl -e '@x=1 .. 17000; for(1..100) { print "@x\n"}' | pv -qL 1000000 | \
+  $PAR --recend '' cat | md5sum &
+nice nice perl -e '@x=1 .. 17000; for(1..100) { print "@x\n"}' | pv -qL 1000000 | \
+  $PAR --recend '' --files cat | parallel -Xj1 cat {}';'rm {} | md5sum &
+nice nice perl -e '@x=1 .. 17000; for(1..100) { print "@x\n"}' | pv -qL 1000000 | \
+  $PAR --recend '' --files cat | parallel -Xj1 cat {}';'rm {} | md5sum &
+nice nice perl -e '@x=1 .. 17000; for(1..100) { print "@x\n"}' | pv -qL 1000000 | \
+  $PAR --recend '' --files --tmpdir /dev/shm cat | parallel -Xj1 cat {}';'rm {} | md5sum &
+nice nice perl -e '@x=1 .. 17000; for(1..100) { print "@x\n"}' | pv -qL 1000000 | \
+  $PAR --recend '' --files --halt-on-error 2 cat | parallel -Xj1 cat {}';'rm {} | md5sum &
+wait
+
+
+
