b84663bac811c66f1762b83659c4cec9431359f6
Ole Tange <ole@tange.dk>
2011-05-28 16:33:22 +0200

    parallel: test added for --onall

---
+++ testsuite/tests-to-run/test60.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVER1=parallel-server3
+SERVER2=parallel-server2
+
+echo '### Test --onall'
+parallel --onall -S parallel@$SERVER2,$SERVER1 '(echo {3} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2 3
+parallel --onall -S parallel@$SERVER2,$SERVER1 -u '(echo {3} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2 3
+
+echo '### Test --nonall'
+parallel --nonall -S parallel@$SERVER2,$SERVER1 'hostname'
+parallel --nonall -S parallel@$SERVER2,$SERVER1 -u 'hostname;sleep 2;hostname'
+
