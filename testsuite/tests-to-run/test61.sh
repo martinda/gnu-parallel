0979c6009e74ea37d277d82fbeb37bbd2a3152af
Ole Tange <ole@tange.dk>
2011-05-31 20:48:00 +0200

    Added test for multiple {} in a composed command

--- testsuite/tests-to-run/test61.sh
+++ testsuite/tests-to-run/test61.sh
@@ -6,3 +6,6 @@ SERVER2=parallel-server2
 echo '### Test --return of weirdly named file'
 stdout parallel --return {} -vv -S $SERVER1 echo '>'{} ::: 'aa<${#}" b'
 rm 'aa<${#}" b'
+
+echo '### Test {} multiple times in different commands'
+seq 1 10|  parallel -v -Xj1 echo {} \; echo {}
