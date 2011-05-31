5377e4d66d6df859bd9959c6eae7ff38c3c3d4bb
Ole Tange <ole@tange.dk>
2011-05-31 19:54:14 +0200

    Added test for {//}

--- testsuite/tests-to-run/test57.sh
+++ testsuite/tests-to-run/test57.sh
@@ -8,6 +8,14 @@ parallel -k echo {//} {} ::: a.jpg a/b.jpg a/b/c.jpg
 parallel -k echo {//} {} ::: /a.jpg /a/b.jpg /a/b/c.jpg
 parallel -k echo {//} {} ::: ./a.jpg ./a/b.jpg ./a/b/c.jpg
 
+echo '### Test {1//}'
+parallel -k echo {1//} {} ::: a a/b a/b/c
+parallel -k echo {1//} {} ::: /a /a/b /a/b/c
+parallel -k echo {1//} {} ::: ./a ./a/b ./a/b/c
+parallel -k echo {1//} {} ::: a.jpg a/b.jpg a/b/c.jpg
+parallel -k echo {1//} {} ::: /a.jpg /a/b.jpg /a/b/c.jpg
+parallel -k echo {1//} {} ::: ./a.jpg ./a/b.jpg ./a/b/c.jpg
+
 echo '### Test --dnr'
 parallel --dnr II -k echo II {} ::: a a/b a/b/c
 
