bd18c891fc1abb78c113615b154f3c18e32e855f
Ole Tange <ole@tange.dk>
2011-05-31 19:53:25 +0200

    Fixed race condition in test

--- testsuite/tests-to-run/test60.sh
+++ testsuite/tests-to-run/test60.sh
@@ -5,7 +5,10 @@ SERVER2=parallel-server2
 
 echo '### Test --onall'
 parallel --onall -S parallel@$SERVER2,$SERVER1 '(echo {3} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2 3
-parallel --onall -S parallel@$SERVER2,$SERVER1 -u '(echo {3} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2 3
+echo '### Test | --onall'
+seq 3 | parallel --onall -S parallel@$SERVER2,$SERVER1 '(echo {3} {2}) | awk \{print\ \$2}' ::: a b c :::: -
+echo '### Test --onall -u'
+parallel --onall -S parallel@$SERVER2,$SERVER1 -u '(echo {3} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2 3 | sort
 
 echo '### Test --nonall'
 parallel --nonall -S parallel@$SERVER2,$SERVER1 'hostname'
