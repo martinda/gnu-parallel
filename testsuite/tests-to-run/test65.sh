c48c5fb7b95e8f959a2b8da558ce117634660440
Ole Tange <ole@tange.dk>
2011-09-09 21:15:00 +0200

    parallel: Implemented --shellquote

--- testsuite/tests-to-run/test65.sh
+++ testsuite/tests-to-run/test65.sh
@@ -5,3 +5,8 @@ cat <<'EOF' | sed -e s/\$SERVER1/$SERVER1/\;s/\$SERVER2/$SERVER2/ | parallel -j1
 echo '### Test --timeout'; 
   parallel -j0 -k --timeout 1 echo {}\; sleep {}\; echo {} ::: 1.1 2.2 3.3 4.4
 EOF
+
+echo '### Test --shellquote'
+cat <<'_EOF' | parallel --shellquote  
+awk -v FS="\",\"" '{print $1, $3, $4, $5, $9, $14}' | grep -v "#" | sed -e '1d' -e 's/\"//g' -e 's/\/\/\//\t/g' | cut -f1-6,11 | sed -e 's/\/\//\t/g' -e 's/ /\t/g
+_EOF
