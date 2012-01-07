dbfb878246a848c054ccd299dc90631ca8520907
Ole Tange <ole@tange.dk>
2012-01-07 02:24:50 +0100

    parallel: retired -g -B -T -U -W -Y

--- testsuite/tests-to-run/test65.sh
+++ testsuite/tests-to-run/test65.sh
@@ -4,6 +4,14 @@
 cat <<'EOF' | sed -e s/\$SERVER1/$SERVER1/\;s/\$SERVER2/$SERVER2/ | parallel -j10 -k -L1
 echo '### Test --timeout'; 
   parallel -j0 -k --timeout 1 echo {}\; sleep {}\; echo {} ::: 1.1 2.2 3.3 4.4
+echo '### Test retired'; 
+  stdout parallel -B;
+  stdout parallel -g;
+  stdout parallel -H;
+  stdout parallel -T;
+  stdout parallel -U;
+  stdout parallel -W;
+  stdout parallel -Y;
 EOF
 
 echo '### Test --shellquote'
