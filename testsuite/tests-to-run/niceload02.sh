513089c881864bb74fd3cfd80c0d4f4c62f4a637
Ole Tange <ole@tange.dk>
2012-03-04 02:24:37 +0100

    Fixed bug #35469: niceload --process" doesn't work after July-2011 release.

--- testsuite/tests-to-run/niceload02.sh
+++ testsuite/tests-to-run/niceload02.sh
@@ -3,3 +3,8 @@
 echo '### Test niceload exit code'
 niceload "perl -e 'exit(3)'" ; echo $? eq 3
 niceload "perl -e 'exit(0)'" ; echo $? eq 0
+
+echo '### Test -p'
+perl -e '$|=1;while($t++<3){sleep(1);print "."}' &
+# The above should be suspended for at least 4 seconds
+stdout /usr/bin/time -f %e niceload -D -l -2 -p $! | perl -ne '$_ > 6 and print "OK\n"'
