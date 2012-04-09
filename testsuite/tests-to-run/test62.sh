f4f5fdd6c67a46320924b5b81199613d87ecdc6b
Ole Tange <ole@tange.dk>
2012-04-09 16:31:05 +0200

    Fixed: bug #36139: --load causes parallel not to finish

--- testsuite/tests-to-run/test62.sh
+++ testsuite/tests-to-run/test62.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo '### Test --joblog with exitval'
-parallel --joblog /tmp/parallel_test_joblog 'sleep {} && echo foo' ::: 100 2>/dev/null &
-sleep 1
+echo '### Test --joblog with exitval and Test --joblog with signal'
+parallel --joblog /tmp/parallel_joblog_exitval 'sleep {} && echo foo' ::: 100 2>/dev/null &
+parallel --joblog /tmp/parallel_joblog_signal 'sleep {}' ::: 100 2>/dev/null &
+sleep 0.5
 killall -6 sleep
-grep -q 134 /tmp/parallel_test_joblog && echo OK
+sleep 0.1
+grep -q 134 /tmp/parallel_joblog_exitval && echo exitval OK
+grep -q '[^0-9]6[^0-9]' /tmp/parallel_joblog_signal && echo signal OK
 
-echo '### Test --joblog with signal'
-parallel --joblog /tmp/parallel_test_joblog 'sleep {}' ::: 100 2>/dev/null &
-sleep 1
-killall -6 sleep
-grep -q '[^0-9]6[^0-9]' /tmp/parallel_test_joblog && echo OK
+rm /tmp/parallel_joblog_exitval /tmp/parallel_joblog_signal
 
-rm /tmp/parallel_test_joblog
