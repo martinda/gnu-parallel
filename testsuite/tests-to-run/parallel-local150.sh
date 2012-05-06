29726eee6ea8915520d38fc7e239f8bb2642aa7c
Ole Tange <ole@tange.dk>
2012-05-06 12:43:40 +0200

    parallel: added --tagstring + man + test. parallel.pod: Elaborated on problems with -u.

--- testsuite/tests-to-run/parallel-local150.sh
+++ testsuite/tests-to-run/parallel-local150.sh
@@ -2,3 +2,10 @@
 
 echo '### Test fix #32191'
 seq 1 150 | parallel -j9 --retries 2 -S localhost,: "/bin/non-existant 2>/dev/null"
+
+echo '### Test --tagstring'
+parallel -j1 -X -v --tagstring a{}b echo  ::: 3 4
+parallel -j1 -k -v --tagstring a{}b echo  ::: 3 4
+parallel -j1 -k -v --tagstring a{}b echo job{#} ::: 3 4
+parallel -j1 -k -v --tagstring ajob{#}b echo job{#} ::: 3 4
+
