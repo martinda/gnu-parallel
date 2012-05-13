1371108e64c07c369a17b7298203c609dbc37e4a
Ole Tange <ole@tange.dk>
2012-05-13 16:03:13 +0200

    Fixed with test: Test bug #35820: sem breaks if $HOME is not writable

--- testsuite/tests-to-run/parallel-local150.sh
+++ testsuite/tests-to-run/parallel-local150.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '### Test fix #32191'
-seq 1 150 | parallel -j9 --retries 2 -S localhost,: "/bin/non-existant 2>/dev/null"
+seq 1 150 | nice nice parallel -j9 --retries 2 -S localhost,: "/bin/non-existant 2>/dev/null"
 
 echo '### Test --tagstring'
 parallel -j1 -X -v --tagstring a{}b echo  ::: 3 4
@@ -9,3 +9,9 @@ parallel -j1 -k -v --tagstring a{}b echo  ::: 3 4
 parallel -j1 -k -v --tagstring a{}b echo job{#} ::: 3 4
 parallel -j1 -k -v --tagstring ajob{#}b echo job{#} ::: 3 4
 
+echo '### Test bug #35820: sem breaks if $HOME is not writable'
+echo 'Workaround: use another writable dir'
+rm -rf /tmp/.parallel
+HOME=/tmp sem echo OK
+HOME=/tmp sem --wait
+HOME=/usr/this/should/fail stdout sem echo should fail
