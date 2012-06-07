38ccd3f193f547969abaff74207c23f1706a9515
Ole Tange <ole@tange.dk>
2012-06-08 01:07:25 +0200

    Fixed with test: bug #36595: silent loss of input with --pipe and --sshlogin.

--- testsuite/tests-to-run/parallel-local20.sh
+++ testsuite/tests-to-run/parallel-local20.sh
@@ -20,7 +20,7 @@ EOF
 chmod 755 /tmp/shebang
 /tmp/shebang
 
-echo '### Test of #! with 2 files as input'
+echo '### Test of #! with 2 files as input (2 columns)'
 cat >/tmp/shebang <<EOF
 #!/usr/local/bin/parallel --shebang -rk --xapply -a /tmp/123 echo
 A
@@ -30,4 +30,5 @@ EOF
 chmod 755 /tmp/shebang
 seq 1 3 >/tmp/123
 /tmp/shebang
-
+echo '### bug #36595: silent loss of input with --pipe and --sshlogin'
+seq 10000 | xargs | parallel --pipe -S 10/localhost cat | wc
