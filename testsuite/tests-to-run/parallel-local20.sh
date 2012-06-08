2c2965301203fa75e65c0ee1fa4fe71d6b89eb8d
Ole Tange <ole@tange.dk>
2012-06-08 23:45:06 +0200

    parallel: --shebang now works even if crazy people name their executable "<> or space.

--- testsuite/tests-to-run/parallel-local20.sh
+++ testsuite/tests-to-run/parallel-local20.sh
@@ -21,14 +21,14 @@ chmod 755 /tmp/shebang
 /tmp/shebang
 
 echo '### Test of #! with 2 files as input (2 columns)'
-cat >/tmp/shebang <<EOF
+cat >'/tmp/she <bang>"' <<EOF
 #!/usr/local/bin/parallel --shebang -rk --xapply -a /tmp/123 echo
 A
 B
 C
 EOF
-chmod 755 /tmp/shebang
+chmod 755 '/tmp/she <bang>"'
 seq 1 3 >/tmp/123
-/tmp/shebang
+'/tmp/she <bang>"'
 echo '### bug #36595: silent loss of input with --pipe and --sshlogin'
 seq 10000 | xargs | parallel --pipe -S 10/localhost cat | wc
