9c8a03bdf2a94cc0e5093f5a5ed2b9f4e7cfb676
Ole Tange <ole@tange.dk>
2012-11-20 00:39:25 +0100

    testsuite: niceload: The most fragile tests split off.

--- testsuite/tests-to-run/niceload01.sh
+++ testsuite/tests-to-run/niceload01.sh
@@ -31,15 +31,6 @@ EOF
 # force load > 10
 while uptime | grep -v age:.[1-9][0-9].[0-9][0-9] >/dev/null ; do (timeout 5 nice burnP6 2>/dev/null &) done
 
-cat <<'EOF' | stdout parallel -j0 -L1
-echo '### --soft -f and test if child is actually suspended and thus takes longer'
-  niceload --soft -t 0.2 -f 0.5 'seq 1000000 | wc;echo This should finish last'
-  (sleep 1; seq 1000000 | wc;echo This should finish first)
-EOF
-
-# force load > 10
-while uptime | grep -v age:.[1-9][0-9].[0-9][0-9] >/dev/null ; do (timeout 5 nice burnP6 2>/dev/null &) done
-
 cat <<'EOF' | stdout parallel -j0 -k -L1
 echo '### -H and --hard'
   niceload  -H -l 9.9 uptime | grep ':.[1-9][0-9].[0-9][0-9],' || echo OK-l9.9
---
+++ testsuite/tests-to-run/niceload03.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# force load > 10
+while uptime | grep -v age:.[1-9][0-9].[0-9][0-9] >/dev/null ; do (timeout 5 nice burnP6 2>/dev/null &) done
+
+cat <<'EOF' | stdout parallel -j0 -L1
+echo '### --soft -f and test if child is actually suspended and thus takes longer'
+  niceload --soft -t 0.2 -f 0.5 'seq 1000000 | wc;echo This should finish last'
+  (sleep 1; seq 1000000 | wc;echo This should finish first)
+EOF
+
