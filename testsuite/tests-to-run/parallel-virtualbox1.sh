b342a1562dae679a4905c2146d7fd8d474ead8bc
Ole Tange <ole@tange.dk>
2012-11-20 00:45:59 +0100

    testsuite/tests-to-run/parallel-virtualbox1.sh: Use whoami instead of tange.

--- testsuite/tests-to-run/parallel-virtualbox1.sh
+++ testsuite/tests-to-run/parallel-virtualbox1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "### These tests requires VirtualBox running with the following images"
-echo "tange@redhat9"
-echo "tange@centos3"
-echo "tange@centos5"
+echo `whoami`"@redhat9"
+echo `whoami`"@centos3"
+echo `whoami`"@centos5"
+echo `whoami`"@freebsd7"
 
 echo "### bug #37589: Red Hat 9 (Shrike) perl v5.8.0 built for i386-linux-thread-multi error"
 cp `which parallel` /tmp/parallel
-stdout parallel --argsep == --basefile /tmp/parallel --tag --nonall -S redhat9.tange.dk,centos3.tange.dk,centos5.tange.dk /tmp/parallel echo ::: OK_if_no_perl_warnings | sort
+stdout parallel -j10 --argsep == --basefile /tmp/parallel --tag --nonall -S redhat9.tange.dk,centos3.tange.dk,centos5.tange.dk,freebsd7.tange.dk /tmp/parallel echo ::: OK_if_no_perl_warnings | sort
