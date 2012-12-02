59a1d67b5983d7c5b93d0d6d4436d3874d3afe62
Ole Tange <ole@tange.dk>
2012-12-03 00:02:51 +0100

    niceload: test + bugfix for --sensor

--- testsuite/tests-to-run/niceload02.sh
+++ testsuite/tests-to-run/niceload02.sh
@@ -9,5 +9,11 @@ while uptime | grep -v age:.[1-9][0-9].[0-9][0-9] >/dev/null ; do (timeout 5 nic
 
 echo '### Test -p'
 perl -e '$|=1;while($t++<3){sleep(1);print "."}' &
-# The above should be suspended for at least 4 seconds
-stdout /usr/bin/time -f %e niceload -D -l 9 -p $! | perl -ne '$_ > 6 and print "OK\n"'
+# The above will normally take 3.6 sec
+# It should be suspended so it at least takes 5 seconds
+stdout /usr/bin/time -f %e niceload -l 8 -p $! | perl -ne '$_ >= 5 and print "OK\n"'
+
+echo "### Test --sensor -l negative"
+timeout 10 nice nice dd iflag=fullblock if=/dev/zero of=/dev/null bs=10G &
+niceload -t 1 --sensor 'free | field 3 | head -3|tail -1' -l -5000000 "free -g;echo more than 5 GB used"
+
