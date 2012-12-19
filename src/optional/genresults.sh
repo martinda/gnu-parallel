86808fed5d36c0c9750d945f62169d71cd8a17e3
Ole Tange <github.com@tange.dk>
2012-12-19 11:42:19 +0100

    optional: --results change to '/' breaks the reading results.

--- src/optional/genresults.sh
+++ src/optional/genresults.sh
@@ -2,5 +2,5 @@
 #
 # Generate the result files used to test the query modules.
 
-../parallel --header : --result testresults/foo_ echo {a} {b} ::: a 1 2 ::: b 0.30 0.40
-../parallel --header : --result testresults/bar_ echo {a} {b} ::: a 5 6 ::: b 0.70 0.80
+../parallel --header : --result testresults/foo echo {a} {b} ::: a 1 2 ::: b 0.30 0.40
+../parallel --header : --result testresults/bar echo {a} {b} ::: a 5 6 ::: b 0.70 0.80
