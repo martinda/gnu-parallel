d5fa05b4250bfa6f620372a6060a18eb8428eb9b
Ole Tange <ole@tange.dk>
2013-03-03 13:45:31 +0100

    Fixed bug #38354: -J profile_name should read from `pwd`/profile_name before ~/.parallel/profile_name

--- testsuite/tests-to-run/parallel-local164.sh
+++ testsuite/tests-to-run/parallel-local164.sh
@@ -2,9 +2,38 @@
 
 # -L1 will join lines ending in ' '
 cat <<'EOF' | parallel -j10 -k -L1
+echo '### bug #38354: -J profile_name should read from `pwd`/profile_name before ~/.parallel/profile_name'
+  echo "echo echo from ./local_test_profile" > local_test_profile; 
+  parallel --profile local_test_profile echo ::: 1; 
+  rm local_test_profile
+
 echo "### Test --delay"
 seq 9 | /usr/bin/time -f %e  parallel -j3 --delay 0.57 true {} 2>&1 | 
   perl -ne '$_ > 5 and print "More than 5 secs: OK\n"'
+
+echo '### test --sshdelay'
+  stdout /usr/bin/time -f %e parallel -j0 --sshdelay 0.5 -S localhost true ::: 1 2 3 | perl -ne 'print($_ > 1.80 ? "OK\n" : "Not OK\n")'
+
+echo '### bug #38299: --resume-failed -k'
+  rm /tmp/joblog-38299; 
+  parallel -k --resume-failed --joblog /tmp/joblog-38299 echo job{#} val {}\;exit {} ::: 0 1 2 3 0 1; 
+  echo try 2. Gives failing - not 0; 
+  parallel -k --resume-failed --joblog /tmp/joblog-38299 echo job{#} val {}\;exit {} ::: 0 1 2 3 0 1; 
+  echo with exit 0; 
+  parallel -k --resume-failed --joblog /tmp/joblog-38299 echo job{#} val {}\;exit 0  ::: 0 1 2 3 0 1; 
+  echo try 2 again. Gives empty; 
+  parallel -k --resume-failed --joblog /tmp/joblog-38299 echo job{#} val {}\;exit {} ::: 0 1 2 3 0 1
+
+echo '### --resume -k'
+  rm -f /tmp/joblog-resume; 
+  parallel -k --resume --joblog /tmp/joblog-resume echo job{}id\;exit {} ::: 0 1 2 3 0 5; 
+  echo try 2 = nothing; 
+  parallel -k --resume --joblog /tmp/joblog-resume echo job{}id\;exit {} ::: 0 1 2 3 0 5; 
+  echo two extra; 
+  parallel -k --resume --joblog /tmp/joblog-resume echo job{}id\;exit 0 ::: 0 1 2 3 0 5 6 7
+
+echo "bug #37694: Empty string argument skipped when using --quote"
+  parallel -q --nonall perl -le 'print scalar @ARGV' 'a' 'b' ''
 
 echo '### Test -k 5'; 
   sleep 5
@@ -35,17 +64,14 @@ seq 1 10 | parallel -j 1 echo | sort
 seq 1 10 | parallel -j 2 echo | sort
 seq 1 10 | parallel -j 3 echo | sort
 
-echo "bug #37694: Empty string argument skipped when using --quote"
-  parallel -q --nonall perl -le 'print scalar @ARGV' 'a' 'b' ''
-
 echo "bug #37956: --colsep does not default to '\t' as specified in the man page."
   printf "A\tB\n1\tone" | parallel --header : echo {B} {A}
 
 echo '### Test --tollef'
-  parallel -k --tollef echo -- 1 2 3 ::: a b c | sort
+  stdout parallel -k --tollef echo -- 1 2 3 ::: a b c | sort
 
 echo '### Test --tollef --gnu'
-  parallel -k --tollef --gnu echo ::: 1 2 3 -- a b c
+  stdout parallel -k --tollef --gnu echo ::: 1 2 3 -- a b c
 
 echo '### Test --gnu'
   parallel -k --gnu echo ::: 1 2 3 -- a b c
@@ -109,31 +135,10 @@ echo '### -k -i -0'
 echo '### -k -0 -i repl'
   printf '1\0002\0003\0004\0005\000' | stdout parallel -k -0 -i repl echo repl OK
 
-echo '### test --sshdelay'
-  stdout /usr/bin/time -f %e parallel -j0 --sshdelay 0.5 -S localhost true ::: 1 2 3 | perl -ne 'print($_ > 1.80 ? "OK\n" : "Not OK\n")'
-
 echo '### Negative replacement strings'
   parallel -X -j1 -N 6 echo {-1}orrec{1} ::: t B X D E c
   parallel -N 6 echo {-1}orrect ::: A B X D E c
   parallel --colsep ' ' echo '{2} + {4} = {2} + {-1}=' '$(( {2} + {-1} ))' ::: "1 2 3 4"
   parallel --colsep ' ' echo '{-3}orrect' ::: "1 c 3 4"
-
-echo '### bug #38299: --resume-failed -k'; 
-  rm /tmp/joblog-38299; 
-  parallel -k --resume-failed --joblog /tmp/joblog-38299 echo job{#} val {}\;exit {} ::: 0 1 2 3 0 1; 
-  echo try 2. Gives failing - not 0; 
-  parallel -k --resume-failed --joblog /tmp/joblog-38299 echo job{#} val {}\;exit {} ::: 0 1 2 3 0 1; 
-  echo with exit 0; 
-  parallel -k --resume-failed --joblog /tmp/joblog-38299 echo job{#} val {}\;exit 0  ::: 0 1 2 3 0 1; 
-  echo try 2 again. Gives empty; 
-  parallel -k --resume-failed --joblog /tmp/joblog-38299 echo job{#} val {}\;exit {} ::: 0 1 2 3 0 1
-
-echo '### --resume -k'; 
-  rm -f /tmp/joblog-resume; 
-  parallel -k --resume --joblog /tmp/joblog-resume echo job{}id\;exit {} ::: 0 1 2 3 0 5; 
-  echo try 2 = nothing; 
-  parallel -k --resume --joblog /tmp/joblog-resume echo job{}id\;exit {} ::: 0 1 2 3 0 5; 
-  echo two extra; 
-  parallel -k --resume --joblog /tmp/joblog-resume echo job{}id\;exit 0 ::: 0 1 2 3 0 5 6 7
 
 EOF
