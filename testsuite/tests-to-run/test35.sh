f1419ef073d64502b80e3670d2f8b8579e6a463f
Ole Tange <ole@tange.dk>
2013-03-23 21:44:17 +0100

    testsuite: test35 more parallel.

--- testsuite/tests-to-run/test35.sh
+++ testsuite/tests-to-run/test35.sh
@@ -7,20 +7,18 @@ SERVER2=parallel@parallel-server2
 
 echo $SERVER2 >~/.parallel/sshloginfile
 
-echo '### Test --wd newtempdir/newdir/tmp/ with space dirs'
-ssh $SERVER2 rm -rf newtempdir
-stdout parallel -k --wd newtempdir/newdir/tmp/ --basefile 1-col.txt --trc {}.6 -S .. -v echo ">"{}.6 ::: './ ab/c"d/ef g' ' ab/c"d/efg' ./b/bar ./b/foo "./ ab /c' d/ ef\"g" ./2-col.txt './a b/cd / ef/efg'
-# A few rmdir errors are OK as we have multiple files in the same dirs
-find . -name '*.6' | sort
+echo '### Test --wd newtempdir/newdir/tmp/ with space dirs'; 
+  ssh $SERVER2 rm -rf newtempdir; 
+  stdout parallel -j9 -k --wd newtempdir/newdir/tmp/ --basefile 1-col.txt --trc {}.6 -S .. -v echo ">"{}.6 ::: './ ab/c"d/ef g' ' ab/c"d/efg' ./b/bar ./b/foo "./ ab /c' d/ ef\"g" ./2-col.txt './a b/cd / ef/efg'; 
+  find . -name '*.6' | sort
 
-echo '### Test --wd /tmp/newtempdir/newdir/tmp/ with space dirs'
-ssh $SERVER2 rm -rf /tmp/newtempdir
-stdout parallel -k --wd /tmp/newtempdir/newdir/tmp/ --basefile 1-col.txt --trc {}.7 -S .. -v echo ">"{}.7 ::: './ ab/c"d/ef g' ' ab/c"d/efg' ./b/bar ./b/foo "./ ab /c' d/ ef\"g" ./2-col.txt './a b/cd / ef/efg'
-# A few rmdir errors are OK as we have multiple files in the same dirs
-find . -name '*.7' | sort
+echo '### Test --wd /tmp/newtempdir/newdir/tmp/ with space dirs'; 
+  ssh $SERVER2 rm -rf /tmp/newtempdir; 
+  stdout parallel -j9 -k --wd /tmp/newtempdir/newdir/tmp/ --basefile 1-col.txt --trc {}.7 -S .. -v echo ">"{}.7 ::: './ ab/c"d/ef g' ' ab/c"d/efg' ./b/bar ./b/foo "./ ab /c' d/ ef\"g" ./2-col.txt './a b/cd / ef/efg'; 
+  find . -name '*.7' | sort
 
 echo '### Test --workdir ...'
-parallel -k --workdir ... --basefile 1-col.txt --trc {}.1 -S .. echo ">"{}.1 ::: 2-col.txt
+parallel -j9 -k --workdir ... --basefile 1-col.txt --trc {}.1 -S .. echo ">"{}.1 ::: 2-col.txt
 find . -name '*.1' | sort
 
 echo '### Test --wd ...'
@@ -28,21 +26,21 @@ parallel -k --wd ... --basefile 1-col.txt --trc {}.2 -S .. -v echo ">"{}.2 ::: 2
 find . -name '*.2' | sort
 
 echo '### Test --wd ... with space dirs'
-stdout parallel -k --wd ... --basefile 1-col.txt --trc {}.3 -S .. -v echo ">"{}.3 ::: './ ab/c"d/ef g' ' ab/c"d/efg' ./b/bar ./b/foo "./ ab /c' d/ ef\"g" ./2-col.txt './a b/cd / ef/efg'
+stdout parallel -j9 -k --wd ... --basefile 1-col.txt --trc {}.3 -S .. -v echo ">"{}.3 ::: './ ab/c"d/ef g' ' ab/c"d/efg' ./b/bar ./b/foo "./ ab /c' d/ ef\"g" ./2-col.txt './a b/cd / ef/efg'
 # A few rmdir errors are OK as we have multiple files in the same dirs
 find . -name '*.3' | sort
 
 echo '### Test --wd tmpdir'
-parallel -k --wd tmpdir --basefile 1-col.txt --trc {}.4 -S .. -v echo ">"{}.4 ::: 2-col.txt
+parallel -j9 -k --wd tmpdir --basefile 1-col.txt --trc {}.4 -S .. -v echo ">"{}.4 ::: 2-col.txt
 find . -name '*.4' | sort
 
 echo '### Test --wd /tmp/ with space dirs'
-stdout parallel -k --wd /tmp/ --basefile 1-col.txt --trc {}.5 -S .. -v echo ">"{}.5 ::: './ ab/c"d/ef g' ' ab/c"d/efg' ./b/bar ./b/foo "./ ab /c' d/ ef\"g" ./2-col.txt './a b/cd / ef/efg'
+stdout parallel -k -j9 --wd /tmp/ --basefile 1-col.txt --trc {}.5 -S .. -v echo ">"{}.5 ::: './ ab/c"d/ef g' ' ab/c"d/efg' ./b/bar ./b/foo "./ ab /c' d/ ef\"g" ./2-col.txt './a b/cd / ef/efg'
 # A few rmdir errors are OK as we have multiple files in the same dirs
 find . -name '*.5' | sort
 
 echo '### Test --wd ... --cleanup'
-stdout parallel -k --wd ... --cleanup -S lo -v echo ">"{}.6 :::  uNiQuE_sTrInG
+stdout parallel -j9 -k --wd ... --cleanup -S lo -v echo ">"{}.6 :::  uNiQuE_sTrInG
 find ~/.parallel/tmp |grep uNiQuE_sTrInG
 
 cd ..
