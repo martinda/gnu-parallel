02bd84afe9ea46e64e70623c49c314d16ba1f389
Ole Tange <ole@tange.dk>
2013-04-08 16:16:40 +0200

    testsuite: Minor speedups.

--- testsuite/tests-to-run/parallel-local11.sh
+++ testsuite/tests-to-run/parallel-local11.sh
@@ -3,13 +3,13 @@
 # Simple jobs that never fails
 # Each should be taking 100s and be possible to run in parallel
 # I.e.: No race conditions, no logins
-cat <<'EOF' | sed -e 's/;$/; /;s/$SERVER1/'$SERVER1'/;s/$SERVER2/'$SERVER2'/' | stdout parallel -j4 -k -L1
+cat <<'EOF' | sed -e 's/;$/; /;s/$SERVER1/'$SERVER1'/;s/$SERVER2/'$SERVER2'/' | stdout parallel -k -j4 -L1
 echo '### Test if we can deal with output > 4 GB'
-  echo | niceload --io 10 parallel -q perl -e '"\$a=\"x\"x1000000;for(0..4300){print \$a}"' | md5sum
-#  echo | parallel -q perl -e '"\$a=\"x\"x1000000;for(0..4300){print \$a}"' | md5sum
+#  echo | niceload --io 10 parallel -q perl -e '"\$a=\"x\"x1000000;for(0..4300){print \$a}"' | md5sum
+  echo | parallel -q perl -e '$a="x"x1000000;for(0..4300){print $a}' | md5sum
 
 echo "### Test Force outside the file handle limit, 2009-02-17 Gave fork error"
-  (echo echo Start; seq 1 20000 | perl -pe 's/^/true /'; echo echo end) | stdout parallel -uj 0 | grep -v 'processes took|adjusting'
+  (echo echo Start; seq 1 20000 | perl -pe 's/^/true /'; echo echo end) | stdout parallel -uj 0 | egrep -v 'processes took|adjusting'
 
 echo '### Test of --retries on unreachable host'
   seq 2 | stdout parallel -k --retries 2 -v -S 4.3.2.1,: echo
--- testsuite/tests-to-run/parallel-local8.sh
+++ testsuite/tests-to-run/parallel-local8.sh
@@ -8,14 +8,13 @@ echo PARALLEL=$PARALLEL
 
 for i in $(seq 2 10); do
   i2=$[i*i]
-  seq $i2 | parallel -j0 --load 300% -kX echo {} |wc
-  seq 1 ${i2}0000 | nice parallel -kj20 --recend "\n" --spreadstdin gzip -9 | zcat | sort -n | md5sum
+  seq $i2 | parallel -j0 --load 300% -kX echo {} | wc
+  seq 1 ${i2}0000 | nice parallel -kj20 --recend "\n" --spreadstdin gzip -1 | zcat | sort -n | md5sum
 done
 
 echo "### Test if --load blocks. Bug.";
-seq 1 1000 | parallel -kj2 --load 300% --recend "\n" --spreadstdin gzip -9 | zcat | sort -n | md5sum
-seq 1 1000 | parallel -kj0 --load 300% --recend "\n" --spreadstdin gzip -9 | zcat | sort -n | md5sum
+seq 1 1000 | parallel -kj2 --load 300% --recend "\n" --spreadstdin gzip -1 | zcat | sort -n | md5sum
+seq 1 1000 | parallel -kj0 --load 300% --recend "\n" --spreadstdin gzip -1 | zcat | sort -n | md5sum
 
-seq 1 1000000 | parallel -kj0 --recend "\n" --spreadstdin gzip -9 | zcat | sort -n | md5sum
-seq 1 1000000 | nice parallel -kj20 --recend "\n" --spreadstdin gzip -9 | zcat | sort -n | md5sum
- 
+seq 1 1000000 | parallel -kj0 --recend "\n" --spreadstdin gzip -1 | zcat | sort -n | md5sum
+seq 1 1000000 | parallel -kj20 --recend "\n" --spreadstdin gzip -1 | zcat | sort -n | md5sum
