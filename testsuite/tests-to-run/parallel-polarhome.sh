e377032c5db8b8fb35265be5575e51083e086fed
Ole Tange <ole@tange.dk>
2013-05-22 00:29:41 +0200

    parallel: Work around for "tcgetattr: Invalid argument" in old OpenSSHs.

--- testsuite/tests-to-run/parallel-polarhome.sh
+++ testsuite/tests-to-run/parallel-polarhome.sh
@@ -7,13 +7,13 @@ POLAR=`parallel echo {}.polarhome.com ::: $P`
 
 echo '### Tests on polarhome machines'
 echo 'Setup on polarhome machines'
-stdout parallel -kj0 ssh {} mkdir -p bin ::: $POLAR >/dev/null &
+stdout parallel -kj0 ssh {} mkdir -p bin ::: $POLAR >/dev/null 2>/dev/null &
 # scp to each polarhome machine do not work. From redhat it works.
 stdout rsync -a `which parallel` redhat.polarhome.com:bin/
 stdout ssh redhat.polarhome.com \
   chmod 755 bin/parallel\; \
-  bin/parallel --tag -kj0 -t --delay 0.2 ssh {} rm -f bin/parallel\\\;scp bin/parallel {}:bin/ ::: $POLAR
+  bin/parallel --tag -kj0 -v --delay 0.2 ssh {} rm -f bin/parallel\\\;scp bin/parallel {}:bin/ ::: $POLAR | sort
 # Now test
 echo 'Run the test on polarhome machines'
-stdout parallel --argsep // -k --tag ssh {} bin/parallel -k echo Works on ::: {} // $POLAR
+stdout parallel --argsep // -k --tag ssh {} bin/parallel -k echo Works on ::: {} // $POLAR | sort
 
--- testsuite/tests-to-run/parallel-virtualbox1.sh
+++ testsuite/tests-to-run/parallel-virtualbox1.sh
@@ -8,4 +8,4 @@ echo `whoami`"@freebsd7"
 
 echo "### bug #37589: Red Hat 9 (Shrike) perl v5.8.0 built for i386-linux-thread-multi error"
 cp `which parallel` /tmp/parallel
-stdout parallel -j10 --argsep == --basefile /tmp/parallel --tag --nonall -S redhat9.tange.dk,centos3.tange.dk,centos5.tange.dk,freebsd7.tange.dk /tmp/parallel echo ::: OK_if_no_perl_warnings | sort
+stdout parallel -kj10 --argsep == --basefile /tmp/parallel --tag --nonall -S redhat9.tange.dk,centos3.tange.dk,centos5.tange.dk,freebsd7.tange.dk /tmp/parallel echo ::: OK_if_no_perl_warnings | sort
