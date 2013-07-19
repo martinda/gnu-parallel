dd1a63782260be3d70d37a704a8b456900051ca6
Ole Tange <ole@tange.dk>
2013-07-19 19:13:00 +0200

    parallel: Use file descriptors instead of STDOUT/STDERR. Fixed up Polarhome tests. Passes testsuite.

--- testsuite/tests-to-run/parallel-local11.sh
+++ testsuite/tests-to-run/parallel-local11.sh
@@ -5,26 +5,38 @@
 # I.e.: No race conditions, no logins
 cat <<'EOF' | sed -e 's/;$/; /;s/$SERVER1/'$SERVER1'/;s/$SERVER2/'$SERVER2'/' | stdout parallel -k -j4 -L1
 echo '### Test if we can deal with output > 4 GB'
-#  echo | niceload --io 10 parallel -q perl -e '"\$a=\"x\"x1000000;for(0..4300){print \$a}"' | md5sum
+##  echo | niceload --io 10 parallel -q perl -e '"\$a=\"x\"x1000000;for(0..4300){print \$a}"' | md5sum
   echo | parallel -q perl -e '$a="x"x1000000;for(0..4300){print $a}' | md5sum
+
+echo '**'
 
 echo "### Test Force outside the file handle limit, 2009-02-17 Gave fork error"
   (echo echo Start; seq 1 20000 | perl -pe 's/^/true /'; echo echo end) | stdout parallel -uj 0 | egrep -v 'processes took|adjusting'
 
+echo '**'
+
 echo '### Test of --retries on unreachable host'
   seq 2 | stdout parallel -k --retries 2 -v -S 4.3.2.1,: echo
+
+echo '**'
 
 echo '### Test race condition on 8 CPU (my laptop)'; 
   seq 1 5000000 > /tmp/parallel_test; 
   seq 1 10 | parallel -k "cat /tmp/parallel_test | parallel --pipe --recend '' -k gzip >/dev/null; echo {}"
 
+echo '**'
+
 echo '### Test exit val - true'; 
   echo true | parallel; 
   echo $?
 
+echo '**'
+
 echo '### Test exit val - false'; 
   echo false | parallel; 
   echo $?
+
+echo '**'
 
 echo '### Test --halt-on-error 0'; 
   (echo "sleep 1;true"; echo "sleep 2;false";echo "sleep 3;true") | parallel -j10 --halt-on-error 0; 
@@ -32,11 +44,15 @@ echo '### Test --halt-on-error 0';
   (echo "sleep 1;true"; echo "sleep 2;false";echo "sleep 3;true";echo "sleep 4; non_exist") | parallel -j10 --halt 0; 
   echo $?
 
+echo '**'
+
 echo '### Test --halt-on-error 1'; 
   (echo "sleep 1;true"; echo "sleep 2;false";echo "sleep 3;true") | parallel -j10 --halt-on-error 1; 
   echo $?; 
   (echo "sleep 1;true"; echo "sleep 2;false";echo "sleep 3;true";echo "sleep 4; non_exist") | parallel -j10 --halt 1; 
   echo $?
+
+echo '**'
 
 echo '### Test --halt-on-error 2'; 
   (echo "sleep 1;true"; echo "sleep 2;false";echo "sleep 3;true") | parallel -j10 --halt-on-error 2; 
@@ -44,11 +60,17 @@ echo '### Test --halt-on-error 2';
   (echo "sleep 1;true"; echo "sleep 2;false";echo "sleep 3;true";echo "sleep 4; non_exist") | parallel -j10 --halt 2; 
   echo $?
 
-echo '### Test last dying print --halt-on-error'; 
+echo '**'
+
+echo '### Test last dying print --halt-on-error 1'; 
   (seq 0 8;echo 0; echo 9) | parallel -j10 -kq --halt 1 perl -e 'sleep $ARGV[0];print STDERR @ARGV,"\n"; exit shift'; 
-  echo $?; 
+  echo exit code $?
+
+echo '### Test last dying print --halt-on-error 2'; 
   (seq 0 8;echo 0; echo 9) | parallel -j10 -kq --halt 2 perl -e 'sleep $ARGV[0];print STDERR @ARGV,"\n"; exit shift'; 
-  echo $?
+  echo exit code $?
+
+echo '**'
 
 echo '### Test slow arguments generation - https://savannah.gnu.org/bugs/?32834'; 
   seq 1 3 | parallel -j1 "sleep 2; echo {}" | parallel -kj2 echo
--- testsuite/tests-to-run/parallel-polarhome.sh
+++ testsuite/tests-to-run/parallel-polarhome.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-P="scosysv centos dragonfly netbsd freebsd solaris openbsd debian aix hpux qnx irix suse minix openindiana mandriva unixware miros raspberrypi hurd ultrix ubuntu redhat"
-P="scosysv centos dragonfly netbsd freebsd solaris openbsd debian aix hpux qnx irix suse minix openindiana mandriva unixware raspberrypi hurd ultrix ubuntu"
-#P="scosysv hpux qnx irix openindiana ultrix"
-POLAR=`parallel echo {}.polarhome.com ::: $P`
+P_ALL="vax freebsd solaris openbsd netbsd debian alpha aix redhat hpux ultrix qnx irix tru64 openindiana suse openstep mandriva ubuntu scosysv unixware dragonfly centos miros hurd minix raspberrypi"
+P_NOTWORKING="vax alpha openstep"
+P_NOTWORKING_YET="ultrix tru64"
+
+P_WORKING="freebsd solaris openbsd netbsd debian aix redhat hpux  qnx irix openindiana suse mandriva ubuntu scosysv unixware dragonfly centos miros hurd minix raspberrypi"
+
+P="$P_WORKING"
+POLAR=`parallel -k echo {}.polarhome.com ::: $P`
+# Avoid the stupid /etc/issue.net banner at Polarhome
 
 echo '### Tests on polarhome machines'
 echo 'Setup on polarhome machines'
-stdout parallel -kj0 ssh {} mkdir -p bin ::: $POLAR >/dev/null 2>/dev/null &
-# scp to each polarhome machine do not work. From redhat it works.
-stdout rsync -a `which parallel` redhat.polarhome.com:bin/
-stdout ssh redhat.polarhome.com \
-  chmod 755 bin/parallel\; \
-  bin/parallel --tag -kj0 -v --delay 0.2 ssh {} rm -f bin/parallel\\\;scp bin/parallel {}:bin/ ::: $POLAR | sort
+stdout parallel -kj0 ssh -oLogLevel=quiet {} mkdir -p bin ::: $POLAR &
+# scp to each polarhome machine do not work. Use cat
+copy_to_host() {
+    H=$1
+    # Avoid the stupid /etc/issue.net banner with -oLogLevel=quiet
+    ssh -oLogLevel=quiet $H rm -f bin/parallel
+    cat `which parallel` | ssh -oLogLevel=quiet $H 'cat > bin/parallel; chmod 755 bin/parallel'
+}
+export -f copy_to_host
+stdout parallel -j0 --timeout 20 --tag -kj0 -v copy_to_host {} ::: $POLAR
 # Now test
-echo 'Run the test on polarhome machines'
-stdout parallel --argsep // -k --tag ssh {} bin/parallel -k echo Works on ::: {} // $POLAR | sort
+echo '### Run the test on polarhome machines'
+stdout parallel -j0 --argsep // -k --tag ssh -oLogLevel=quiet {} bin/perl bin/parallel -k echo Works on ::: {} // $POLAR
+
 
