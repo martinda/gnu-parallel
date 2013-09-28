358b9091f6bc15b01ed3bb57ecdd0861b1a0ef81
Ole Tange <ole@tange.dk>
2013-09-28 17:44:54 +0200

    parallel: Fixed bug #40137: SHELL not bash: Warning when exporting funcs.

--- testsuite/tests-to-run/parallel-local-ssh1.sh
+++ testsuite/tests-to-run/parallel-local-ssh1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat <<'EOF' | sed -e s/\$SERVER1/$SERVER1/\;s/\$SERVER2/$SERVER2/ | parallel -j10 -k -L1
+cat <<'EOF' | sed -e s/\$SERVER1/$SERVER1/\;s/\$SERVER2/$SERVER2/ | stdout parallel -j10 -k -L1
 echo '### Test bug #34241: --pipe should not spawn unneeded processes'
   seq 5 | ssh csh@lo parallel -k --block 5 --pipe -j10 cat\\\;echo Block_end
 
@@ -8,6 +8,10 @@ echo '### --env _'
   fUbAr="OK FUBAR" parallel -S csh@lo --env _ echo '$fUbAr $DISPLAY' ::: test
 echo '### --env _ with explicit mentioning of normally ignored var $DISPLAY'
   fUbAr="OK FUBAR" parallel -S csh@lo --env DISPLAY,_ echo '$fUbAr $DISPLAY' ::: test
+
+echo 'bug #40137: SHELL not bash: Warning when exporting funcs'
+  . <(printf 'myfunc() {\necho $1\n}'); export -f myfunc; parallel --env myfunc -S lo myfunc ::: no_warning
+  . <(printf 'myfunc() {\necho $1\n}'); export -f myfunc; SHELL=/bin/sh parallel --env myfunc -S lo myfunc ::: warning
 
 echo '### --filter-hosts --slf <()'
   parallel --nonall --filter-hosts --slf <(echo localhost) echo OK
