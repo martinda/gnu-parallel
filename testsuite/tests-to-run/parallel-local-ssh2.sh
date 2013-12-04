6a71285bb626f0d8457a725eaf15939172810fbd
Ole Tange <ole@tange.dk>
2013-12-04 17:12:09 +0100

    parallel: Fixed bug if --wd does not exists. Incl test.

--- testsuite/tests-to-run/parallel-local-ssh2.sh
+++ testsuite/tests-to-run/parallel-local-ssh2.sh
@@ -4,4 +4,10 @@ cat <<'EOF' | sed -e s/\$SERVER1/$SERVER1/\;s/\$SERVER2/$SERVER2/ | stdout paral
 echo '### --filter-hosts --slf <()'
   parallel --nonall --filter-hosts --slf <(echo localhost) echo OK
 
+echo '### --wd no-such-dir'
+  stdout parallel --wd /no-such-dir -S csh@localhost echo ::: 1; echo Exit code $?
+  stdout parallel --wd /no-such-dir -S tcsh@localhost echo ::: 1; echo Exit code $?
+  stdout parallel --wd /no-such-dir -S parallel@localhost echo ::: 1; echo Exit code $?
+
+
 EOF
