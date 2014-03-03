583e6fb4b73552c3cfa7157099c409491173b53e
Ole Tange <ole@tange.dk>
2014-03-03 19:26:19 +0100

    Fixed bug #38908: niceload: Ctrl-C should resume jobs if using -p

--- testsuite/tests-to-run/niceload03.sh
+++ testsuite/tests-to-run/niceload03.sh
@@ -3,13 +3,44 @@
 # force load > 10
 while uptime | grep -v age:.[1-9][0-9].[0-9][0-9] >/dev/null ; do (timeout 5 nice burnP6 2>/dev/null &) done
 
-cat <<'EOF' | stdout parallel -j0 -L1
+int() {
+  perl -pe 's/(\d+\.\d*)/int($1)/e'
+}
+export -f int
+
+cat <<'EOF' | stdout parallel -kj0 -L1
 # The seq 10000000 should take > 1 cpu sec to run.
 echo '### --soft -f and test if child is actually suspended and thus takes longer'
-  niceload --soft -f 0.5 'seq 20000000 | wc;echo This should finish last'
-  (sleep 1; seq 20000000 | wc;echo This should finish first)
+  niceload --soft -f 0.5 'seq 20000000 | wc;echo This should finish last' & 
+  (sleep 1; seq 20000000 | wc;echo This should finish first) & 
+  wait
+
 echo '### niceload with no arguments should give no output'
   niceload
+
+echo '### Test -t and -s'
+  niceload -v -t 1 -s 2 sleep 4.5
+
+echo 'bug #38908: niceload: Ctrl-C/TERM should resume jobs if using -p'
+  # This should take 10 seconds to run + delay from niceload
+  # niceload killed after 1 sec => The delay from niceload should be no more than 1 second
+  stdout /usr/bin/time -f %e perl -e 'for(1..100) { select(undef, undef, undef, 0.1); } print "done\n"' | int & 
+  niceload -vt 1 -s 10 -p $! & 
+  export A=$!; 
+  sleep 2; 
+  kill -s TERM $A; 
+  wait
+
+echo 'bug #38908: niceload: Ctrl-C should resume jobs if using -p'
+  # This should take 10 seconds to run + delay from niceload
+  # niceload killed after 1 sec => The delay from niceload should be no more than 1 second
+  stdout /usr/bin/time -f %e perl -e 'for(1..100) { select(undef, undef, undef, 0.1); } print "done\n"' | int & 
+  niceload -vt 1 -s 10 -p $! & 
+  export A=$!; 
+  sleep 2; 
+  kill -s INT $A; 
+  wait
+
 EOF
 
 # TODO test -f + -t
--- testsuite/tests-to-run/parallel-virtualbox1.sh
+++ testsuite/tests-to-run/parallel-virtualbox1.sh
@@ -17,7 +17,7 @@ ping -c 1 freebsd7.tange.dk >/dev/null 2>&1
 
 echo "### bug #37589: Red Hat 9 (Shrike) perl v5.8.0 built for i386-linux-thread-multi error"
 cp `which parallel` /tmp/parallel
-stdout parallel -kj10 --argsep == --basefile /tmp/parallel --tag --nonall -S redhat9.tange.dk,centos3.tange.dk,centos5.tange.dk,freebsd7.tange.dk /tmp/parallel echo ::: OK_if_no_perl_warnings | sort
+stdout parallel -kj10 --argsep == --basefile /tmp/parallel --tag --nonall -S redhat9.tange.dk,centos3.tange.dk,centos5.tange.dk,freebsd7.tange.dk /tmp/parallel --no-notice echo ::: OK_if_no_perl_warnings | sort
 
 #VBoxManage controlvm CentOS3-root:centos3 savestate
 VBoxManage controlvm CentOS5-root:centos5 savestate
