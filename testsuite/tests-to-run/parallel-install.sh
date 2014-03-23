84ab59fb97e25559288bc66bbb3b2d306a1ac7ba
Ole Tange <ole@tange.dk>
2014-03-23 17:41:15 +0100

    Fixed bug #41749: make install fails

---
+++ testsuite/tests-to-run/parallel-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo '### Test installation missing pod2*'
+
+parallel which ::: pod2html pod2man pod2texi pod2pdf | sudo parallel mv {} {}.hidden
+
+cd ~/privat/parallel
+stdout make dist | perl -pe 's/\d{8}/00000000/g'
+LAST=$(ls *tar.gz | tail -n1)
+cd /tmp
+rm -rf parallel-20??????/
+tar xf ~/privat/parallel/$LAST
+cd parallel-20??????/
+./configure && sudo stdout make install | perl -pe 's/\d{8}/00000000/g'
+
+parallel which {}.hidden ::: pod2html pod2man pod2texi pod2pdf | sudo parallel mv {} {.}
