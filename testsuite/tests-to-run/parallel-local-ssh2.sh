9c5832148200e23ec6eda85d248f152b5193a60e
Ole Tange <ole@tange.dk>
2014-07-15 15:58:31 +0200

    Fixed bug #42725: csh with \n in variables

--- testsuite/tests-to-run/parallel-local-ssh2.sh
+++ testsuite/tests-to-run/parallel-local-ssh2.sh
@@ -10,4 +10,13 @@ echo '### --wd no-such-dir - tcsh'
   stdout parallel --wd /no-such-dir -S tcsh@localhost echo ::: "ERROR IF PRINTED"; echo Exit code $?
 echo '### --wd no-such-dir - bash'
   stdout parallel --wd /no-such-dir -S parallel@localhost echo ::: "ERROR IF PRINTED"; echo Exit code $?
+
+echo '### bug #42725: csh with \n in variables'
+  not_csh() { echo This is not csh/tcsh; }; 
+  export -f not_csh; 
+  parallel --env not_csh -S csh@lo not_csh ::: 1; 
+  parallel --env not_csh -S tcsh@lo not_csh ::: 1; 
+  parallel --env not_csh -S parallel@lo not_csh ::: 1; 
+
+
 EOF
